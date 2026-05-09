b18baa27bb8306c7ca9b127ab692e474b2fd07d1
DarioN370 <netobis2015@gmail.com>
2026-05-09 16:14:00 -0300

    init: Iniciando a criacao da logica da function que vai fazer o calculo do orcamento para que eu possa rodar os testes, usei algumas constantes para nao precisar ficar deixando valores estaticos em caso de mudancas de cenarios / Alem disso, adicionei o arquivo README.md

---
+++ orcamento.js
@@ -0,0 +1,24 @@
+function calcularOrcamento(valorPecas, valorMaoDeObra, desconto) {
+    const percentualImposto = 0.05; // 5% de imposto (ISS)
+    const descontoMaximo = 0.15;   // Desconto máximo de 15%
+
+    // Regra de Negócio: O desconto não pode ser maior que 15% (0.15)
+    if (desconto > descontoMaximo) {
+        return "Erro: Desconto acima do limite permitido";
+    } // Se o valor do desconto for maior que o descontoMaximo, vai dar erro
+
+    // Regra de Negócio: Valores não podem ser negativos
+    if (valorPecas < 0 || valorMaoDeObra < 0 || desconto < 0) {
+        return "Erro: Valores inválidos";
+    } // Se qualquer valor for menor que 0 (negativo), vai dar erro
+
+    // calculos para puxar na function
+    const subtotal = valorPecas + valorMaoDeObra;
+    const valorComDesconto = subtotal - (subtotal * desconto);
+    const imposto = valorComDesconto * percentualImposto;
+
+    // Retorna apenas o valor total final
+    return valorComDesconto + imposto;
+}
+
+module.exports = calcularOrcamento;
